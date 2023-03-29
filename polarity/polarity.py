828d65135bb59c6b309e7404618cbd5417fb5a4c
Pedrom Zadeh <zadeh@virginia.edu>
2023-03-29 17:10:20 -0400

    defined probabilistic patch addition model

--- polarity/polarity.py
+++ polarity/polarity.py
@@ -1,28 +1,41 @@
 import numpy as np
 
-from polarity.mvgaussian import MVGaussian
 
+def mvg_noise_patch(cell, mp, cov_ii, cov_ij):
+    
+    def pick_c(kind="mp-only"):
+        from helper_functions import helper_functions as hf
 
-def mvg_noise_patch(cell, dphi_dt):
+        grad_x, grad_y, _ = hf.compute_gradients(cell.phi, cell.simbox.dx)
+        grad_phi = np.array([grad_x, grad_y])
+        grad_phi_norm = np.sqrt(np.sum(grad_phi * grad_phi, axis=0))
+        n_hat = -grad_phi / grad_phi_norm
 
-    cntrs = cell.contour[0]  # [y, x]
-    probs = np.fabs(dphi_dt)
-    cntrs_probs = np.array([probs[int(y), int(x)] for y, x in cntrs])
-    cntrs_probs[np.isnan(cntrs_probs)] = 0
-    cntrs_probs /= np.sum(cntrs_probs)
+        cntr = cell.contour[0][:, ::-1]
+        cntr_n_hats = np.array(
+            [[n_hat[0][int(y), int(x)], n_hat[1][int(y), int(x)]] for x, y in cntr]
+        )
+        cntr_shifted = np.array(
+            [
+                [x + l * nx, y + l * ny]
+                for x, y, nx, ny in np.hstack((cntr, cntr_n_hats))
+            ]
+        )
+
+        l = 5
+        p = 1 - mp
+        cntr_probs = np.array([p[int(y), int(x)] for x, y in cntr_shifted])
+        cntr_probs /= cntr_probs.sum()
+
+        return cntr[np.random.choice(range(len(cntr)), p=cntr_probs)]
 
     # pick a contour point according to cntrs_probs
-    c = cntrs[np.random.choice(range(len(cntrs)), p=cntrs_probs)][::-1]
+    c = pick_c()
     means = np.array(c)
-    cov = np.array([[8, 4], [4, 8]])
+    cov = np.array([[cov_ii, cov_ij], [cov_ij, cov_ii]])
+    N_mesh = cell.simbox.N_mesh
 
-    d = np.linspace(0, 200, 200)
-    x, y = np.meshgrid(d, d)
-    X = np.array(list(zip(x.flatten(), y.flatten())))
-
-    MVG_f = MVGaussian(means, cov)
-
-    return MVG_f.pdf(X).reshape(200, 200)
+    return cell.mvg_gen.pdf(means, cov).reshape(N_mesh, N_mesh)
 
 
 def dFpol_dP(cell):
@@ -32,7 +45,7 @@ def dFpol_dP(cell):
     return 2 * nu * (1 - P_tot / P_tot_target) * (-1 / P_tot_target)
 
 
-def adaptive_pol_field(cell, dphi_dt, mp):
+def adaptive_pol_field(cell, dphi_dt, mp, args):
     """
     Returns the change in polarization field experienced in one timestep.
 
@@ -54,17 +67,18 @@ def adaptive_pol_field(cell, dphi_dt, mp):
     dt = cell.simbox.dt
     dx = cell.simbox.dx
 
-    # parameters relevant to field update
     D = cell.D
-    beta = cell.beta
-    tau = cell.tau  # x8 to get minutes
-    tau_x = cell.tau_mp
+    tau_x, beta, tau_N, tau, sigma, tau_add, n = args
 
-    noise = np.sqrt(4 * D**2 * dt / dx**2) * cell.rng.randn(*phi.shape)
+    if n % tau_add == 0:
+        noise = sigma * mvg_noise_patch(cell, mp, cov_ii=20, cov_ij=0)
+    else:
+        noise = 0
+
+    # noise = np.sqrt(4 * D**2 * dt / dx**2) * cell.rng.randn(*phi.shape)
     return (
         (dt * phi * beta * dphi_dt)
         - (dt / tau * p_field)
-        + (noise * phi)
+        + (dt / tau_N * noise * phi)
         - (dt / tau_x * mp * phi)
-        - (dt * dFpol_dP(cell))
     )
