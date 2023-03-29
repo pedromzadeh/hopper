02779aa4cf8d55c616087621f3c80713934a9768
Pedrom Zadeh <zadeh@virginia.edu>
2023-03-29 17:03:30 -0400

    redefine MVG such that support and object are defined once

--- polarity/mvgaussian.py
+++ polarity/mvgaussian.py
@@ -2,24 +2,22 @@ import numpy as np
 
 
 class MVGaussian:
-    def __init__(self, means, cov):
+    def __init__(self, X):
+        self.X = X.reshape(-1, 1, 2)
 
-        self.means = means
-        self.cov = cov
-        self.rank = len(means)
-        self._is_sym_pos_def()
-        pre_factor = np.power(2 * np.pi, self.rank) * np.linalg.det(cov)
-        self.pre_factor = 1 / np.sqrt(pre_factor)
+    def pdf(self, means, cov):
+        rank = len(means)
+        self._is_sym_pos_def(cov)
+        pre_factor = np.power(2 * np.pi, rank) * np.linalg.det(cov)
+        pre_factor = 1 / np.sqrt(pre_factor)
 
-    def pdf(self, X):
-        inv_cov = np.linalg.inv(self.cov)
-        X = X.reshape(-1, 1, 2)
-        term = (X - self.means) @ inv_cov @ (X - self.means).reshape(-1, 2, 1)
-        return self.pre_factor * np.exp(-1 / 2 * term)
+        inv_cov = np.linalg.inv(cov)
+        term = (self.X - means) @ inv_cov @ (self.X - means).reshape(-1, 2, 1)
+        return pre_factor * np.exp(-1 / 2 * term)
 
-    def _is_sym_pos_def(self):
-        sym = np.all(self.cov == self.cov.T)
-        pos_def = np.all(np.linalg.eigvals(self.cov) > 0)
+    def _is_sym_pos_def(self, cov):
+        sym = np.all(cov == cov.T)
+        pos_def = np.all(np.linalg.eigvals(cov) > 0)
         if not (sym and pos_def):
             raise ArithmeticError(
                 "Covariance matrix must be symmetric positive definite."
